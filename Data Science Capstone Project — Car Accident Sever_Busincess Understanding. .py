29bf1645f2f711e73fe604a7956c6c2e9f5a15dd
salahger81 <68323717+salahger81@users.noreply.github.com>
2020-09-15 20:25:23 +0200

    Car Accident Sever_Busincess Understanding

---
+++ Data Science Capstone Project — Car Accident Sever_Busincess Understanding. .py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # IBM Data Science Capstone Report 
+
+# # Seattle Car Accident Severity # 
+
+# ## 1-  Business Understanding
+
+# The Seattle government is going to prevent avoidable car accidents by employing methods that alert drivers, health system, and police to remind them to be more careful in critical situations.
+# 
+
+# In most cases, not paying enough attention during driving, abusing drugs and alcohol or driving at very high speed are the main causes of occurring accidents that can be prevented by enacting harsher regulations. Besides the aforementioned reasons, weather, visibility, or road conditions are the major uncontrollable factors that can be prevented by revealing hidden patterns in the data and announcing warning to the local government, police and drivers on the targeted roads.
+# 
+
+# The target audience of the project is local Seattle government, police, rescue groups, and last but not least, car insurance institutes. The model and its results are going to provide some advice for the target audience to make insightful decisions for reducing the number of accidents and injuries for the city.
+# 
+
+# In[ ]:
+
+
+
+
